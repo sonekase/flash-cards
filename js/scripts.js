48081024c0ca2500b2cda692b58829793c3ae792
Lan Dam <ldam77@yahoo.com>
2018-05-29 14:38:20 -0700

    add scripts.js and code to highlight selected paragraph.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("p").click(function(){
+    $("p").removeClass();
+    $(this).addClass("highlight");
+  });
+
+  // $("#p2").click(function() {
+  //   $("p").removeClass();
+  //   $("#p2").addClass("highlight");
+  // });
+  //
+  // $("#p3").click(function() {
+  //   $("p").removeClass();
+  //   $("#p3").addClass("highlight");
+  // });
+});
